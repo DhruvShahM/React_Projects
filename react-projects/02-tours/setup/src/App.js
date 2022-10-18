285deb4b751f9439e60f3656f16c77cf198f5233
DhruvShah <dhruvshah78223@gmail.com>
2022-10-18 19:42:11 +0530

    2 - tours and 3 - reviews project setup.

--- react-projects/02-tours/setup/src/App.js
+++ react-projects/02-tours/setup/src/App.js
@@ -1,11 +1,57 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
+import {FaGithubSquare} from 'react-icons/fa'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading,setLoading]=useState(true);
+  const [tours,setTours]=useState([]);
+
+  const removeTour=(id)=>{
+      const newTours=tours.filter((tour)=>tour.id!==id);
+      setTours(newTours);
+  }
+
+  const fecthTours= async()=>{
+    setLoading(true);
+
+    try {
+      setLoading(false);
+      const response=await fetch(url);
+      const tours=await response.json();
+      console.log("par",tours);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(()=>{
+    fecthTours();
+  },[])
+
+if(loading){
+  return <Loading/>
+}
+
+
+if(tours.length===0)
+{
+  return <main>
+    <div className="title">
+      <h2>no tours left</h2>
+      <button className='btn' onClick={fecthTours}>Refresh button</button>
+    </div>
+  </main>
+}
+  return <main>
+    <Tours tours={tours} removeTour={removeTour}/>
+  </main>
 }
 
 export default App

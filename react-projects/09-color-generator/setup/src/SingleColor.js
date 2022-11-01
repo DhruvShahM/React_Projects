ed14e7d2f86632a20e7a4c4fbd6f0a99bd406302
DhruvShah <dhruvshah78223@gmail.com>
2022-11-01 19:10:05 +0530

    6. Tabs 7. Slider 8. Lorem Ipsum Generator 9. Color Shades Generator projects code completed.

--- react-projects/09-color-generator/setup/src/SingleColor.js
+++ react-projects/09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,34 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({rgb,weight,index,hexColor}) => {
+  //hex color getting by value
+  console.log(hexColor);
+  const [alert,setAlert]=useState(false);
+  const bcg=rgb.join(',');
+  const hex=rgbToHex(...rgb);
+  const hexValue=`#${hexColor}`;
+
+  //remove the message of the 'Copied to clipboard' after 3 seconds
+  useEffect(()=>{
+    const timeout=setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return ()=>clearTimeout(timeout);
+  },[alert])
+
+  return <article className={`color ${index>10 && 'color-light'}`} style={{backgroundColor:`rgb(${bcg})`}} onClick={()=>{
+    setAlert(true);
+    navigator.clipboard.writeText(hexValue);
+  }}>
+    <p className='percent-value'>{weight}%</p>
+    {/* hex color getting by values */}
+    {/* <p className="color-value">{hex}</p>
+     */}
+    <p className="color-value">{hexValue}</p>
+    {alert && <p className="alert">Copied to clipboard</p>}
+
+  </article>
 }
 
 export default SingleColor
